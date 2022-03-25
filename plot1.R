c31eb393e716b3b23b7912d76309901a0f26a614
HizbanSheik <100220056+HizbanSheik@users.noreply.github.com>
2022-03-25 11:49:36 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,18 +1,17 @@
-library("data.table")
-path <- getwd()
-download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
-              , destfile = paste(path, "dataFiles.zip", sep = "/"))
-unzip(zipfile = "dataFiles.zip")
+dir()
+Source_Classification_Code <- read.table("C:/Users/hizba/Downloads/exdata_data_NEI_data/Source_Classification_Code.rds", header=TRUE, quote="\"")
+View(Source_Classification_Code)
+summarySCC_PM25 <- read.table("C:/Users/hizba/Downloads/exdata_data_NEI_data/summarySCC_PM25.rds", header=TRUE, quote="\"")
+View(summarySCC_PM25)
 
-SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
-NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
-# Prevents histogram from printing in scientific notation
-NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
-
-totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
-
-barplot(totalNEI[, Emissions]
-        , names = totalNEI[, year]
-        , xlab = "Years", ylab = "Emissions"
-        , main = "Emissions over the Years")
+emissions_by_year <- NEI %>%
+     group_by(year) %>%
+     summarize(total_emissions = sum(Emissions))
+ 
+with(emissions_by_year, barplot(total_emissions, names.arg = year, xlab = "Year", ylab = "PM2.5 Emissions", main = "Total PM2.5 Emissions over the Years"))
+ 
+dev.copy(png,'plot1.png') 
+dev.off()
